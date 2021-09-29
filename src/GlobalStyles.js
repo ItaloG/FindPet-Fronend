33dd0ca25c440831e97f63d3e749046da9b207fd
Melissa Melo <melissamelo529@gmail.com>
2021-09-29 04:31:07 -0300

    atualização páginas feed e encontrar

---
+++ src/GlobalStyles.js
@@ -0,0 +1,71 @@
+import styled, { createGlobalStyle } from "styled-components";
+import { MenuItem } from "./components/Header/styles";
+
+export const GlobalStyles = createGlobalStyle`
+    :root {
+        --primary: #C98822;
+        --secondary: #FFB853;
+        --logoColor: #935B00;
+        --headerColor: #F6E4CB;
+        --text: #000000;
+        --backgroundColor: #F3F3F3;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', 'sans-serif';
+    }
+
+    body {
+        overflow-x: hidden;
+    }
+
+    /* @media(max-width: 1000px) {
+        ${MenuItem} {
+            background-color: aquamarine;
+        }
+    } */
+`;
+
+export const Body = styled.div`
+    width: 100vw;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    background-color: var(--backgroundColor);
+    overflow-x: hidden;
+`;
+
+export const Container = styled.div`
+    padding: 20px;
+    border-radius: 15px;
+    box-shadow: 0px 0px 15px #cfcfcf;
+    margin-top: 75px;
+    width: auto;
+    height: auto;
+    background-color: #ffffff;
+`;
+
+export const ContainerCenter = styled.div`
+    padding-top: 40px;
+    margin-top: 75px;
+    width: 940px;
+    height: auto;
+`;
+
+export const FlexColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: auto;
+`;
+
+export const FlexRow = styled.div`
+    display: flex;
+    width: 100%;
+    height: auto;
+`;
