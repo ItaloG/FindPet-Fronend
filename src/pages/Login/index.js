4df2c7499a045b3659f741e3276539a6011ecd06
ITALO GABRIEL MESQUITA PEREI <20272538@cfp127aluno.local>
2021-10-05 09:44:25 -0300

    consumindo api para fazer login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -17,6 +17,11 @@ import SpinnerLoading from "../../components/SpinnerLoading/indes";
 function Login() {
   let history = useHistory();
 
+  const [login, setLogin] = useState({
+    email: "",
+    senha: "",
+  });
+
   const [instituicao, setInstituicao] = useState({
     nome: "",
     tipoEstabelecimento: 0,
@@ -51,77 +56,78 @@ function Login() {
 
   const handleCepInstituicao = (e) => {
     let cep = e.target.value;
-    cep = mascaraCep(cep)
-    setInstituicao({ ...instituicao, cep: cep })
-  }
+    cep = mascaraCep(cep);
+    setInstituicao({ ...instituicao, cep: cep });
+  };
 
   const handleCellInstuicao = (e) => {
     let cell = e.target.value;
-    cell = macaraCell(cell)
-    setInstituicao({ ...instituicao, celular: cell })
-  }
+    cell = macaraCell(cell);
+    setInstituicao({ ...instituicao, celular: cell });
+  };
 
   const handleTellInstuicao = (e) => {
     let tell = e.target.value;
-    tell = mascaraTell(tell)
-    setInstituicao({ ...instituicao, telefone: tell })
-  }
+    tell = mascaraTell(tell);
+    setInstituicao({ ...instituicao, telefone: tell });
+  };
 
   const handleCepUsuario = (e) => {
     let cep = e.target.value;
-    cep = mascaraCep(cep)
+    cep = mascaraCep(cep);
     setUsuario({ ...usuario, cep: cep });
-  }
+  };
 
   const handleCellUsuario = (e) => {
     let cell = e.target.value;
-    cell = macaraCell(cell)
-    setUsuario({ ...usuario, celular: cell })
-  }
+    cell = macaraCell(cell);
+    setUsuario({ ...usuario, celular: cell });
+  };
 
   const handleTellUsuario = (e) => {
     let tell = e.target.value;
-    tell = mascaraTell(tell)
-    setUsuario({ ...usuario, telefone: tell })
-  }
+    tell = mascaraTell(tell);
+    setUsuario({ ...usuario, telefone: tell });
+  };
 
   useEffect(() => {
     const getEndereco = async (cep) => {
       const dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
       const endereco = await dados.json();
-      setInstituicao({ ...instituicao, logradouro: endereco.logradouro })
-    }
+      setInstituicao({ ...instituicao, logradouro: endereco.logradouro });
+    };
 
     if (instituicao.cep.length === 9) {
       getEndereco(instituicao.cep);
     }
-
   }, [instituicao.cep]);
 
   useEffect(() => {
     const getEndereco = async (cep) => {
       const dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
       const endereco = await dados.json();
-      setUsuario({ ...usuario, logradouro: endereco.logradouro })
-    }
+      setUsuario({ ...usuario, logradouro: endereco.logradouro });
+    };
 
     if (usuario.cep.length === 9) {
       getEndereco(usuario.cep);
     }
-
   }, [usuario.cep]);
 
   const handleInputInstituicao = (e) => {
-    setInstituicao({ ...instituicao, [e.target.id]: e.target.value })
-  }
+    setInstituicao({ ...instituicao, [e.target.id]: e.target.value });
+  };
 
   const handleInputUsuario = (e) => {
-    setUsuario({ ...usuario, [e.target.id]: e.target.value })
-  }
+    setUsuario({ ...usuario, [e.target.id]: e.target.value });
+  };
+
+  const handleInputLogin = (e) => {
+    setLogin({ ...login, [e.target.id]: e.target.value });
+  };
 
   const handleClose = () => {
     setIscadastrando(false);
-
   };
 
   const handleCloseInstituicao = () => {
@@ -137,7 +143,7 @@ function Login() {
       logradouro: "",
       numero: "",
       complemento: "",
-    })
+    });
     setIsInstituicao(false);
   };
 
@@ -153,7 +159,7 @@ function Login() {
       logradouro: "",
       numero: "",
       complemento: "",
-    })
+    });
     setIsUsuario(false);
   };
 
@@ -177,27 +183,34 @@ function Login() {
       } = instituicao;
 
       if (tipoEstabelecimento === 0) {
-        return alert("escolha um tipo de estabelecimento primeiro")
+        return alert("escolha um tipo de estabelecimento primeiro");
       }
-      if (!nome || !cnpj || !email || !senha || !cep || !logradouro || !numero) {
+      if (
+        !nome ||
+        !cnpj ||
+        !email ||
+        !senha ||
+        !cep ||
+        !logradouro ||
+        !numero
+      ) {
         return alert("faltam alguns dados");
       }
       if (!telefone && !celular) {
-        return alert("Você precisa informar um telefone ou celular")
+        return alert("Você precisa informar um telefone ou celular");
       }
 
-      const response = await api.post("/cadastro/instituicao", instituicao)
+      const response = await api.post("/cadastro/instituicao", instituicao);
       console.log(response.data);
 
       return history.push("/home");
-
     } catch (error) {
       console.error(error);
       alert(error.response.data.error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   const handleSubmitUsuario = async (e) => {
     e.preventDefault();
@@ -221,22 +234,50 @@ function Login() {
         return alert("faltam alguns dados");
       }
       if (!telefone && !celular) {
-        return alert("Você precisa informar um telefone ou celular")
+        return alert("Você precisa informar um telefone ou celular");
       }
 
       const response = await api.post("/cadastro/usuario", usuario);
       console.log(response.data);
 
-      return history.push("/home")
-
+      return history.push("/home");
     } catch (error) {
       console.error(error);
-      alert(error.response.data)
-    }
-    finally {
+      alert(error.response.data);
+    } finally {
       setIsLoading(false);
     }
-  }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { email, senha } = login;
+
+    console.log(login);
+
+    if (!email || !senha) {
+      console.log("caiu aqui");
+      return alert("Você precisa informar um email e senha.");
+    }
+
+    setIsLoading(true);
+
+    try {
+      const response = await api.post("/login/usuario", login);
+
+      localStorage.setItem("@user", JSON.stringify(response.data));
+
+      setIsLoading(false);
+
+      return history.push("/home");
+    } catch (error) {
+      console.log("caiu aqui");
+      console.error(error);
+      setIsLoading(false);
+      alert("Usuário e/ou senha incorretos");
+    }
+  };
 
   return (
     <Container>
@@ -250,16 +291,28 @@ function Login() {
         <div className="texto">
           <p>
             {" "}
-            Te ajudamos a encontrar e ajudar ONG's, petshops, veterinarios, canis
-            e animais desaparecidos{" "}
+            Te ajudamos a encontrar e ajudar ONG's, petshops, veterinarios,
+            canis e animais desaparecidos{" "}
           </p>
         </div>
         <img src={fundo} alt="cachoro" />
       </main>
       <section>
-        <FormContainer>
-          <Input placeholder="E-mail" type="email" id="email" required />
-          <Input placeholder="Senha" type="password" id="senha" required />
+        <FormContainer onSubmit={handleLogin}>
+          <Input
+            placeholder="E-mail"
+            handler={handleInputLogin}
+            type="email"
+            id="email"
+            required
+          />
+          <Input
+            placeholder="Senha"
+            handler={handleInputLogin}
+            type="password"
+            id="senha"
+            required
+          />
           <div className="check">
             <div>
               <input type="checkbox" placeholder="teste" className="caixa" />
@@ -267,15 +320,12 @@ function Login() {
             </div>
             <p>esqueceu a senha?</p>
           </div>
-          <button
-            className="btnEntrar"
-            onClick={(e) => {
-              e.preventDefault();
-              history.push("/home");
-            }}
-          >
-            Entrar
-          </button>
+          {isLoading ? (
+            <SpinnerLoading />
+          ) : (
+            <button className="btnEntrar">Entrar</button>
+          )}
+
           <hr />
 
           <button
@@ -329,23 +379,27 @@ function Login() {
               <option value={3}>Veterinário</option>
               <option value={4}>Petshop</option>
             </select>
-            <Input type="text"
+            <Input
+              type="text"
               placeholder="CNPJ"
               id="cnpj"
               value={instituicao.cnpj}
-              handler={handleInputInstituicao} />
+              handler={handleInputInstituicao}
+            />
             <Input
               type="email"
               placeholder="E-mail"
               id="email"
               value={instituicao.email}
-              handler={handleInputInstituicao} />
+              handler={handleInputInstituicao}
+            />
             <Input
               type="text"
               placeholder="Senha"
               id="senha"
               value={instituicao.senha}
-              handler={handleInputInstituicao} />
+              handler={handleInputInstituicao}
+            />
             <Input
               type="text"
               placeholder="Telefone"
@@ -353,7 +407,8 @@ function Login() {
               maxLength="9"
               id="telefone"
               value={instituicao.telefone}
-              handler={handleTellInstuicao} />
+              handler={handleTellInstuicao}
+            />
             <Input
               type="text"
               placeholder="Celular"
@@ -361,39 +416,40 @@ function Login() {
               pattern="\(?\d{2}\) ?9?\d{4}-?\d{4}"
               maxLength="15"
               value={instituicao.celular}
-              handler={handleCellInstuicao} />
+              handler={handleCellInstuicao}
+            />
             <Input
               type="text"
               placeholder="CEP"
               id="cep"
               pattern="(\d{5})-(\d{3})*"
               value={instituicao.cep}
-              handler={handleCepInstituicao} />
+              handler={handleCepInstituicao}
+            />
             <Input
               type="text"
               placeholder="Rua / Avenida"
               id="logradouro"
               value={instituicao.logradouro}
-              handler={handleInputInstituicao} />
+              handler={handleInputInstituicao}
+            />
             <div className="endereco">
               <Input
                 type="text"
                 placeholder="Número"
                 id="numero"
                 value={instituicao.numero}
-                handler={handleInputInstituicao} />
+                handler={handleInputInstituicao}
+              />
               <Input
                 type="text"
                 placeholder="Complemento"
                 id="complemento"
                 value={instituicao.complemento}
-                handler={handleInputInstituicao} />
+                handler={handleInputInstituicao}
+              />
             </div>
-            {isLoading ? (
-              <SpinnerLoading />
-            ) :
-              <button>Cadastrar-se</button>
-            }
+            {isLoading ? <SpinnerLoading /> : <button>Cadastrar-se</button>}
           </CadastroInstituicoes>
         </Modal>
       )}
@@ -406,31 +462,36 @@ function Login() {
               placeholder="Nome"
               id="nome"
               value={usuario.nome}
-              handler={handleInputUsuario} />
+              handler={handleInputUsuario}
+            />
             <Input
               type="text"
               placeholder="CPF"
               id="cpf"
               value={usuario.cpf}
-              handler={handleInputUsuario} />
+              handler={handleInputUsuario}
+            />
             <Input
               type="email"
               placeholder="E-mail"
               id="email"
               value={usuario.email}
-              handler={handleInputUsuario} />
+              handler={handleInputUsuario}
+            />
             <Input
               type="text"
               placeholder="Senha"
               id="senha"
               value={usuario.senha}
-              handler={handleInputUsuario} />
+              handler={handleInputUsuario}
+            />
             <Input
               type="text"
               placeholder="Telefone"
               id="telefone"
               value={usuario.telefone}
-              handler={handleTellUsuario} />
+              handler={handleTellUsuario}
+            />
             <Input
               type="text"
               placeholder="Celular"
@@ -438,39 +499,40 @@ function Login() {
               pattern="\(?\d{2}\) ?9?\d{4}-?\d{4}"
               maxLength="15"
               value={usuario.celular}
-              handler={handleCellUsuario} />
+              handler={handleCellUsuario}
+            />
             <Input
               type="text"
               placeholder="CEP"
               id="cep"
               pattern="(\d{5})-(\d{3})*"
               value={usuario.cep}
-              handler={handleCepUsuario} />
+              handler={handleCepUsuario}
+            />
             <Input
               type="text"
               placeholder="Rua/Avenida"
               id="logradouro"
               value={usuario.logradouro}
-              handler={handleInputUsuario} />
+              handler={handleInputUsuario}
+            />
             <div className="endereco">
               <Input
                 type="text"
                 placeholder="Número"
                 id="numero"
                 value={usuario.numero}
-                handler={handleInputUsuario} />
+                handler={handleInputUsuario}
+              />
               <Input
                 type="text"
                 placeholder="Complemento"
                 id="complemento"
                 value={usuario.complemento}
-                handler={handleInputUsuario} />
+                handler={handleInputUsuario}
+              />
             </div>
-            {isLoading ? (
-              <SpinnerLoading />
-            ) :
-              <button>Cadastrar-se</button>
-            }
+            {isLoading ? <SpinnerLoading /> : <button>Cadastrar-se</button>}
           </CadastroUsuario>
         </Modal>
       )}
