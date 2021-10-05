8febda69b0378c5c72858f1524f774788fde6a9d
MELISSA GABRIELLE DE MELO <19180122@CFP127ALUNO.LOCAL>
2021-10-05 11:18:16 -0300

    adição do componente slider, quase concluído

--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -2,25 +2,73 @@ import styled from 'styled-components';
 
 export const SliderContainer = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     width: 940px;
     height: 450px;
     background-color: #fff;
     margin-bottom: 20px;
     border: solid 2px #cfcdca;
     border-radius: 13px;
-`;
 
-export const BannerContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 85%;
-    height: 80%;
-    margin-top: 20px;
-    overflow: hidden;
-    > img {
-        width: 100%;
-        height: auto;
+    > .container-items {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        width: 850px;
+        height: 360px;
+        margin-top: 20px;
+        margin-left: 45px;
+        overflow-x: hidden;
+
+        > .item {
+            width: 850px;
+            height: 360px;
+            transition: all 2s;
+            /* max-height: 360px; */
+
+            > img {
+                width: 850px;
+                height: 360px;
+            }
+
+            :first-child {
+                margin-left: -850px;
+            }
+        }
+    }
+
+    > .action-button {
+        z-index: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50px;
+
+        :hover {
+            cursor: pointer;
+        }
+    }
+
+    > .slider-buttons {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1;
+        width: 940px;
+        height: 20px;
+        position: absolute;
+        margin-top: 405px;
+
+        > .choice-slide-button {
+            margin-right: 5px;
+            margin-left: 5px;
+            width: 15px;
+            height: 15px;
+            border-radius: 50%;
+
+            :hover {
+                cursor: pointer;
+            }
+        }
     }
 `;
